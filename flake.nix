7432b0d742cd1f4a002ef6918fbe35e1895acd9d
Jiyu Zhou <carassius1014@gmail.com>
2022-12-08 22:49:48 +0900

    Provide lule as a flake

--- flake.nix
+++ flake.nix
@@ -9,31 +9,43 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, fenix, nixpkgs, utils }: 
-  utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs.outPath {
-        config = { allowUnfree = true; };
-        inherit system;
-        overlays = [
-          fenix.overlay
-        ];
-      };
-    in {
-      defaultPackage = pkgs.mkShell {
-        buildInputs =  with pkgs; [
-          (rust-nightly.latest.withComponents [
-            "cargo"
-            "clippy-preview"
-            "rust-src"
-            "rust-std"
-            "rustc"
-            "rustfmt-preview"
-          ])
-          rust-analyzer
-          rls
-        ];
-      };
-    }
-  );
+  outputs = { self, fenix, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs.outPath {
+          config = { allowUnfree = true; };
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+        toolchain = fenix.packages.${system}.minimal.toolchain;
+        platform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        lule = platform.buildRustPackage {
+          pname = "lule";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "pastel-0.8.0" =
+                "1dnyqwy3n3ww7yma4rq21cgnf7r2136h56ask28hv7vdaynbam2c";
+            };
+          };
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ cargo nixfmt rust-analyzer ];
+        };
+
+        packages.default = lule;
+
+        apps.default = {
+          type = "app";
+          program = "${lule}/bin/lule";
+        };
+      });
 }
